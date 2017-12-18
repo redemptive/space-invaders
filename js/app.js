48a5691ba3f4b357e9cb80e483889c401bee0668
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2017-12-18 13:38:08 +0000

    Added alien sprites

--- js/app.js
+++ js/app.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
 	var alienRows = 2;
 	var alienColumns = 4;
 	var alienHeight = 100;
-	//68 = right, 65 = left
-	var keyMap = {68: false, 65: false};
+	//68 = right, 65 = left, 71 = g(fire)
+	var keyMap = {68: false, 65: false, 71: false};
 	var interval = setInterval(gameLoop, 20);
 
 	function init() {
 		for (var i = 0; i < alienRows; i++) {
 			$(".container").append("<div id=\"" + i + "\" class=\"row\"></div>");
 			for (var j = 0; j < alienColumns; j++) {
-				$("#" + i).append("<div class=\"alien col-md-" + (12 / alienColumns) + "\"></div>");
-				$("#" + i).css("height", alienHeight + "px");
+				$("#" + i).append("<div class=\"alien col-md-" + (12 / alienColumns) + "\"><img src=\"assets/alien.png\" class=\" alien-img img-responsive\"></div>");
 			}
 		}
-		$("body").append("<div id=\"player\"></div>");
+		$("body").append("<div id=\"player\"></div>")
+		$("#player").append("<img src=\"assets/player.png\" height=\"40\">");
 	}
 
 	$(document).keydown(function(e) {
