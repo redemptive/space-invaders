d643e630e8c9dfb8959e1455c847050d6ca3a38e
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2017-12-18 14:03:58 +0000

    Allowed the player to shoot

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
 	//68 = right, 65 = left, 71 = g(fire)
 	var keyMap = {68: false, 65: false, 71: false};
 	var interval = setInterval(gameLoop, 20);
+	var bullet = "";
+	var bulletTemplate = "<div id=\"bullet\" style=\"height: 25px; width: 10px; z-index: 2; position: absolute; bottom: 10px; background-color: #FF0000\"></div>"
 
 	function init() {
 		for (var i = 0; i < alienRows; i++) {
@@ -32,6 +34,17 @@ $(document).ready(function() {
 			player.css("left", player.position().left + 10);
 		} else if (keyMap[65] && player.position().left > 10) {
 			player.css("left", player.position().left - 10);
+		} else if (keyMap[71] && bullet == "") {
+			$("body").append(bulletTemplate);
+			bullet = $("#bullet");
+			bullet.css("left", player.position().left);
+		}
+		if (bullet != "") {
+			if (bullet.position().top < 0) {
+				bullet.remove();
+				bullet = "";
+			}
+			bullet.css("top", bullet.position().top - 10);
 		}
 	}
 
